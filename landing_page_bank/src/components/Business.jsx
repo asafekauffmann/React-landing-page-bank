70fcb0e817d7811a40b1ce9066703fbfbaabf297
asafe <asafe.kauffmann@llam.tec.br>
2024-06-21 18:41:41 -0300

    Start Update Bussines

--- landing_page_bank/src/components/Business.jsx
+++ landing_page_bank/src/components/Business.jsx
@@ -1,10 +1,28 @@
-import { features } from "../constants";
-import style, { layout } from "../style";
-import Button from './Button'; 
+ import { features } from "../constants";
+ import style, { layout } from "../style";
+ import Button from './Button'; 
 
 
 const Business = () => (
-  <h2>Hi bro</h2>
+  <section id="features" className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={style.heading2}>
+        You do the business, <br className="sm:block hidden" /> we’ll handle
+        the money.
+      </h2>
+      <p className={`${style.paragraph} max-w-[470px] mt-5`}>
+        With the right credit card, you can improve your financial life by
+        building credit, earning rewards and saving money. But with hundreds
+        of credit cards on the market.
+      </p>
+      <Button style={`mt-10`}/>
+    </div>
+
+    <div className={`${layout.sectionImg} flex-col`}>
+
+    </div>
+
+  </section>
 )
 
 export default Business;
